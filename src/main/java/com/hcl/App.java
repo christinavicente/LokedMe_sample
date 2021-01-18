9504290db4c358f564c9ebfa019d4dc39c7af676
christinavicente <76453287+christinavicente@users.noreply.github.com>
2021-01-18 09:25:07 -0500

    updated delete line from list.txt

--- src/main/java/com/hcl/App.java
+++ src/main/java/com/hcl/App.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 package com.hcl;
 
 import sun.reflect.generics.tree.Tree;
@@ -141,4 +142,149 @@ public class App
         System.out.println("finished copying file");
     }
 
+=======
+package com.hcl;
+
+import sun.reflect.generics.tree.Tree;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.*;
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+    public static void main( String[] args ) throws IOException{
+        TreeSet<String> filenames = new TreeSet<>();
+        String listFilePath ="src/storedfiles/list.txt";
+        File listedFiles= new File(listFilePath);
+        Scanner scanner=new Scanner(System.in);
+        String dataPath="src/storedfiles/";
+        filehandling fh= new filehandling(dataPath);
+        filenames=initTree(listedFiles,fh);
+        mainMenu(filenames,scanner,dataPath,fh);
+    }
+
+    public static void mainMenu(TreeSet<String> files, Scanner sc,
+                                String path,filehandling fh) throws IOException {
+        int code;
+        do {
+            System.out.println("Choose option: \n 1.) see all files \n " +
+                    "2.) more options \n 3.) close program ");
+            code=sc.nextInt();
+            switch (code){
+                case 1:
+                    readFiles(files);
+                    break;
+                case 2:
+                    System.out.println("loading more options");
+                    moreOptions(files,sc,path,fh);
+                    break;
+                case 3:
+                    System.out.println("thank you! Have a good day!");
+                    closing(fh,files);
+                    break;
+                default:
+                    System.out.println("invalid code, please enter 1,2, or 3");
+            }
+        }while (code!=3);
+        //closing(bw,files);
+    }
+    private static void moreOptions(TreeSet<String> files, Scanner sc,String path, filehandling fh) throws IOException {
+        String temp;
+        int code;
+        do {
+            System.out.println("Choose option: \n 1.) add a file \n " +
+                    "2.) delete a file \n 3.) search for a file \n" +
+                    "4.) return to main menu");
+            code=sc.nextInt();
+            switch(code){
+                case 1:
+
+                    addOne(files, path, sc, fh);
+                    break;
+                case 2:
+                    System.out.println("enter file name to be deleted");
+                    sc.nextLine();
+                    temp=sc.nextLine();
+                    removeOne(files,temp,path,fh);
+                    break;
+                case 3:
+                    System.out.println("enter file to search");
+                    sc.nextLine();
+                    temp=sc.nextLine();
+                    if(fh.findFile(temp)){
+                        System.out.println("file has been found");
+                    }else {
+                        System.out.println("no such file is in the directory");
+                    }
+                    break;
+                case 4:
+                    System.out.println("returning to main menu");
+                    break;
+                default:
+                    System.out.println("invalid code, please enter 1,2, or 3");
+            }
+        }while (code!=4);
+
+    }
+
+    private static void closing(filehandling fh, TreeSet<String> files) throws IOException {
+        fh.editList(files);
+
+    }
+    private static void readFiles(TreeSet<String> files){
+        for(String file: files){
+            System.out.println(file);
+        }
+    }
+
+    private static TreeSet<String> initTree( File list,filehandling fh) throws IOException {
+        TreeSet<String> files=fh.createTreeSet(list);
+        return files;
+    }
+
+
+
+    private static TreeSet<String> removeOne(TreeSet<String> files, String name, String path,
+                                             filehandling fh) throws IOException {
+        if(fh.findFile(name)) {
+            String filePath = path.concat(name);
+            File deletedFile = new File(filePath);
+            files.remove(name);
+            fh.removeElement(name);
+            fh.deleteFile(deletedFile, files);
+        }else {
+            System.out.println("the file is not in the repository");
+        }
+
+        return files;
+    }
+    private static TreeSet<String> addOne(TreeSet<String> files,  String path,
+                                          Scanner scanner, filehandling fh) throws IOException {
+        String temp;
+        System.out.println("enter file name: ");
+        scanner.nextLine();
+        temp=scanner.nextLine();
+        while(fh.findFile(temp)){
+            System.out.println("This file already exists, please enter a new name");
+            temp=scanner.nextLine();
+        }
+        files.add(temp);
+        fh.populateArrayList(temp);
+        fh.createFile(temp);
+        return files;
+    }
+
+    private static void copyFile(String path,filehandling fh, String name) throws IOException {
+        System.out.println("copying file to current repository");
+        fh.copyFile(path,name);
+        System.out.println("finished copying file");
+    }
+
+>>>>>>> c7589a939b132efd833e3984d68cf1a6c5996038
 }
--- src/main/java/com/hcl/filehandling.java
+++ src/main/java/com/hcl/filehandling.java
@@ -1,3 +1,134 @@
+<<<<<<< HEAD
+package com.hcl;
+
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.ArrayList;
+import java.util.TreeSet;
+
+public class filehandling {
+    private String defaultPath,localPath;
+    private FileWriter fileWriter;
+    private ArrayList<String> altFileList;
+    private int leng;
+    public filehandling( String pathname){
+
+        defaultPath=pathname;
+        localPath=defaultPath.concat("list.txt");
+        altFileList=new ArrayList<>(50);
+    }
+    public File createFile(String name) throws IOException {
+        File file;
+        String newPath=defaultPath.concat(name);
+        String localPath=defaultPath.concat("list.txt");
+        file = new File(newPath);
+        if (!file.exists()) {
+            Files.createFile(Paths.get(newPath));
+        }
+        Files.write(
+                Paths.get(localPath),
+                name.getBytes(),
+                StandardOpenOption.CREATE,
+                StandardOpenOption.APPEND
+        );
+        return file;
+    }
+    public File copyFile(String oldPath, String name) throws IOException {
+        String localPath=defaultPath.concat(name);
+        File oldFile=new File(oldPath);
+        File newFile=new File(localPath);
+        fileWriter=new FileWriter(localPath);
+        StringBuilder stringBuilder=new StringBuilder();
+        FileInputStream fileInputStream = new FileInputStream(oldFile);
+        int r = 0;
+        while ((r = fileInputStream.read()) != -1) {
+            stringBuilder.append((char) r);
+        }
+        String fileInput = stringBuilder.toString();
+        if (!newFile.exists()) {
+            Files.createFile(Paths.get(localPath));
+        }
+        fileWriter.write(fileInput);
+        return newFile;
+    }
+    public void deleteFile(File file, TreeSet<String> list) throws IOException {
+        file.delete();
+        editList(list);
+
+
+    }
+    public ArrayList<String> getArrayList(){
+        return altFileList;
+    }
+    public void editList(TreeSet<String> list) throws IOException {
+        //String localPath=defaultPath.concat("list.txt");
+        StringBuilder stringBuilder=new StringBuilder("");
+
+        for(String item:list){
+            stringBuilder=stringBuilder.append(item);
+            stringBuilder= stringBuilder.append('\n');
+            leng=item.length();
+
+        }
+        for(int i=0;i<leng;i++){
+            stringBuilder=stringBuilder.append(' ');
+        }
+        stringBuilder= stringBuilder.append('\n');
+        String tempFile=stringBuilder.toString();
+        Files.write(
+                Paths.get(localPath),
+                tempFile.getBytes(),
+                StandardOpenOption.CREATE,
+                StandardOpenOption.WRITE
+        );
+
+
+    }
+
+    public TreeSet<String> createTreeSet(File listfile) throws IOException {
+        String temp,fullList;
+        TreeSet<String> files= new TreeSet<String>();
+        FileInputStream fileInputStream = new FileInputStream(listfile);
+        int r = 0;int n=0;
+        StringBuilder stringBuilder = new StringBuilder();
+        while ((r = fileInputStream.read()) != -1) {
+            stringBuilder.append((char) r);
+        }
+        fullList=stringBuilder.toString();
+
+        while(fullList.indexOf('\n')!=-1){
+            temp=fullList.substring(n,fullList.indexOf('\n'));
+            if(!temp.contains(" ")) {
+                files.add(temp);
+                populateArrayList(temp);
+            }
+            fullList=fullList.substring(fullList.indexOf('\n')+1);
+        }
+        files.add(fullList);
+        populateArrayList(fullList);
+
+        return files;
+    }
+    public  void populateArrayList(String item){
+        altFileList.add(item);
+    }
+    public boolean findFile( String file){
+
+        return altFileList.contains(file);
+    }
+    public void removeElement(String item){
+        int i=0;
+        while((i<altFileList.size())&&(!altFileList.get(i).equals(item))){
+            i++;
+        }
+        if(altFileList.get(i).equals(item)){
+            altFileList.remove(i);
+        }
+    }
+=======
 package com.hcl;
 
 import oracle.jrockit.jfr.StringConstantPool;
@@ -128,4 +259,5 @@ public class filehandling {
             altFileList.remove(i);
         }
     }
+>>>>>>> c7589a939b132efd833e3984d68cf1a6c5996038
 }
